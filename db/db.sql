24f5d2cd619e29a6a9d5519b7c217e98dd77daf5
ImDenisc <danisatorrecucart@gmail.com>
2025-02-04 21:02:44 +0100

    ADD mm tables

--- db/db.sql
+++ db/db.sql
@@ -7,27 +7,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
--- USUARIOS
-
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE `users`(
-    `id_user` int(20) UNSIGNED NOT NULL,
-    `username` varchar(50) NOT NULL,
-    `pwd` varchar(50) NOT NULL,
-    `email` varchar(50) NOT NULL,
-    `img_perfil` varchar(255) NOT NULL,
-    `dir` varchar(100) NOT NULL,
-    `sexo` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `users`(`id_user`, `username`, `pwd`, `email`, `img_perfil`, `dir`, `sexo`) VALUES
-(1, 'user1', 'user1pwd', 'user1@gmail.com', 'img1.jpg', 'dir1', 'masculino'),
-(2, 'user2', 'user2pwd', 'user2@gmail.com', 'img2.jpg', 'dir2', 'femenino'),
-(3, 'user3', 'user3pwd', 'user3@gmail.com', 'img3.jpg', 'dir3', 'femenino'),
-(4, 'user4', 'user4pwd', 'user4@gmail.com', 'img4.jpg', 'dir4', 'masculino'),
-(5, 'user5', 'user5pwd', 'user5@gmail.com', 'img5.jpg', 'dir5', 'masculino');
-
 -- DIRECCION
 
 DROP TABLE IF EXISTS direccion;
@@ -47,59 +26,6 @@ INSERT INTO `direccion`(`id_dir`, `ciudad`, `calle`, `pais`, `cp`) VALUES
 (4, 'ciudad4', 'calle4', 'pais4', 'cp4'),
 (5, 'ciudad5', 'calle5', 'pais5', 'cp5');
 
--- FAVORITOS
-
-DROP TABLE IF EXISTS favoritos;
-
-CREATE TABLE `favoritos`(
-    `id_fav` int(20) UNSIGNED NOT NULL,
-    `user_fav` int(20) UNSIGNED NOT NULL,
-    `producto_fav` int(20) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `favoritos`(`id_fav`, `user_fav`, `producto_fav`) VALUES
-(1, 1, 1),
-(2, 4, 2),
-(3, 2, 3),
-(4, 3, 4),
-(5, 1, 5);
-
--- COMPRAS
-
-DROP TABLE IF EXISTS compras;
-
-CREATE TABLE `compras`(
-    `id_compra` int(20) UNSIGNED NOT NULL,
-    `user_compra` int(20) UNSIGNED NOT NULL,
-    `producto_compra` int(20) UNSIGNED NOT NULL,
-    `fecha_compra` varchar(10) NOT NULL,
-    `unidades` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-INSERT INTO `compras`(`id_compra`, `user_compra`, `producto_compra`, `fecha_compra`, `unidades`) VALUES
-(1, 1, 1, '01/01/2021', 1),
-(2, 5, 2, '01/01/2021', 1),
-(3, 2, 3, '01/01/2021', 1),
-(4, 3, 4, '01/01/2021', 1),
-(5, 5, 5, '01/01/2021', 1);
-
--- CARRITO
-
-DROP TABLE IF EXISTS carrito;
-
-CREATE TABLE `carrito`(
-    `id_carrito` int(20) UNSIGNED NOT NULL,
-    `user_carrito` int(20) UNSIGNED NOT NULL,
-    `producto_carrito` int(20) UNSIGNED NOT NULL,
-    `cantidad_carrito` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `carrito` (`id_carrito`, `user_carrito`, `producto_carrito`, `cantidad_carrito`) VALUES
-(1, 1, 1, 1),
-(2, 2, 2, 1),
-(3, 3, 3, 4),
-(4, 4, 4, 2),
-(5, 5, 5, 1);
-
 -- PRODUCTOS
 
 DROP TABLE IF EXISTS productos;
@@ -118,15 +44,16 @@ CREATE TABLE `productos`(
     `descripcion` varchar(1000) NOT NULL,
     `condicion` varchar(20) NOT NULL,
     `stock` int(50) NOT NULL,
-    `precio` int(5) NOT NULL
+    `precio` int(5) NOT NULL,
+    `img_producto` varchar(255) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `productos` (`id_producto`, `id_vendedor`, `marca`, `categoria`, `tipo`, `nom_prod`, `sexo_prod`, `talla`, `entrega`, `oferta`, `descripcion`, `condicion`, `stock`, `precio`) VALUES
-(1, 1, 1, 1, 1, 'producto1', 'masculino', 'M', 'persona', 'no', 'descripcion1', 'nuevo', 3, 10),
-(2, 2, 2, 2, 2, 'producto2', 'femenino', 'S', 'domicilio', 'si', 'descripcion2', 'desgastado', 5, 20),
-(3, 3, 3, 3, 3, 'producto3', 'femenino', 'L', 'tienda', 'no', 'descripcion3', 'roto', 3, 30),
-(4, 4, 4, 1, 4, 'producto4', 'masculino', 'XL', 'persona', 'si', 'descripcion4', 'bueno', 1, 40),
-(5, 5, 5, 2, 5, 'producto5', 'masculino', 'M', 'tienda', 'no', 'descripcion5', 'nuevo', 1, 50);
+INSERT INTO `productos` (`id_producto`, `id_vendedor`, `marca`, `categoria`, `tipo`, `nom_prod`, `sexo_prod`, `talla`, `entrega`, `oferta`, `descripcion`, `condicion`, `stock`, `precio`, `img_producto`) VALUES
+(1, 1, 1, 1, 1, 'producto1', 'masculino', 'M', 'persona', 'no', 'descripcion1', 'nuevo', 3, 10, `prod1.jpg`),
+(2, 2, 2, 2, 2, 'producto2', 'femenino', 'S', 'domicilio', 'si', 'descripcion2', 'desgastado', 5, 20, `prod2.jpg`),
+(3, 3, 3, 3, 3, 'producto3', 'femenino', 'L', 'tienda', 'no', 'descripcion3', 'roto', 3, 30, `prod3.jpg`),
+(4, 4, 4, 1, 4, 'producto4', 'masculino', 'XL', 'persona', 'si', 'descripcion4', 'bueno', 1, 40, `prod4.jpg`),
+(5, 5, 5, 2, 5, 'producto5', 'masculino', 'M', 'tienda', 'no', 'descripcion5', 'nuevo', 1, 50, `prod5.jpg`);
 
 -- PRODUCT IMAGES
 
@@ -259,26 +186,6 @@ INSERT INTO `devoluciones` (`id_dev`, `user_dev`, `producto_dev`, `motivo`, `fec
 (4, 3, 2, 3, '03/10/2024', 'Completado'),
 (5, 1, 1, 1, '03/10/2024', 'En proceso');
 
--- PAGOS
-
-DROP TABLE IF EXISTS pagos;
-
-CREATE TABLE `pagos`(
-    `id_pago` int(20) UNSIGNED NOT NULL,
-    `metodo_pago` int(20) UNSIGNED NOT NULL,
-    `producto_pago` int(20) UNSIGNED NOT NULL,
-    `user_pago` int(20) UNSIGNED NOT NULL,
-    `fecha_pago` varchar(10) NOT NULL,
-    `coste` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `pagos` (`id_pago`, `metodo_pago`, `producto_pago`, `user_pago`, `fecha_pago`, `coste`) VALUES
-(1, 1, 1, 1, '12/12/2024', 10),
-(2, 2, 2, 2, '12/12/2024', 20),
-(3, 3, 3, 3, '12/12/2024', 30),
-(4, 4, 4, 4, '12/12/2024', 40),
-(5, 5, 5, 5, '12/12/2024', 50);
-
 -- MOTIVOS
 
 DROP TABLE IF EXISTS motivos;
@@ -294,27 +201,6 @@ INSERT INTO `motivos` (`id_motivo`, `motivo`) VALUES
 (3, 'Disconforme'),
 (4, 'Talla equivocada'),
 (5, 'Otro');
-
--- CHAT
-
-DROP TABLE IF EXISTS chats;
-
-CREATE TABLE `chats`(
-    `id_chat` int(20) UNSIGNED NOT NULL,
-    `cliente_chat` int(20) UNSIGNED NOT NULL,
-    `vendedor_chat` int(20) UNSIGNED NOT NULL,
-    `producto_chat` int(20) UNSIGNED NOT NULL,
-    `mensaje` varchar(255) NOT NULL,
-    `fecha_msg` varchar(10) NOT NULL,
-    `hora_msg` varchar(5) NOT NULL 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `chats` (`id_chat`, `cliente_chat`, `vendedor_chat`, `producto_chat`, `mensaje`, `fecha_msg`, `hora_msg`) VALUES
-(1, 1, 5, 1, 'mensaje1', '03/02/2025', '12:38'),
-(2, 2, 3, 2, 'mensaje2', '03/02/2025', '12:38'),
-(3, 3, 4, 3, 'mensaje3', '01/02/2025', '12:38'),
-(4, 4, 2, 4, 'mensaje4', '09/01/2025', '12:38'),
-(5, 5, 1, 5, 'mensaje5', '12/11/2024', '12:38');
 
 -- SUBASTAS
 
@@ -382,34 +268,71 @@ INSERT INTO `players` (`id_player`, `nom_player`) VALUES
 (4, 'Stephen Curry'),
 (5, 'Devin Booker');
 
+--
+-- MANY TO MANY TABLES
+--
+
+-- PRODUCTOS & MARCAS
+
+DROP TABLE IF EXISTS productos_marcas;
+
+CREATE TABLE `productos_marcas`(
+    -- PRODUCTOS
+    `id_producto` int(20) UNSIGNED NOT NULL,
+    -- MARCAS
+    `id_marca` int(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `productos_marcas` (`id_producto`,`id_marca`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5);
+
+-- PRODUCTOS & CATEGIRIAS
+
+DROP TABLE IF EXISTS productos_categorias;
+
+CREATE TABLE `productos_categorias`(
+    -- PRODUCTOS
+    `id_producto` int(20) UNSIGNED NOT NULL,
+    -- CATEGORIAS
+    `id_categoria` int(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `productos_categorias` (`id_producto`,`id_categoria`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 3);
+
+-- PRODUCTOS & TIPO
+
+DROP TABLE IF EXISTS `productos_tipo`(
+    -- PRODUCTOS
+    `id_producto` int(20) UNSIGNED NOT NULL,
+    -- TIPO
+    `id_tipo` int(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `productos_tipo` (`id_producto`,`id_tipo`) VALUES
+(1, 1),
+(5, 2),
+(1, 2),
+(2, 7),
+(2, 5),
+(3, 8);
+
 -- ALTER TABLES
-
--- PRIMARY KEYS
-
--- USERS
-
-ALTER TABLE `users`
-    ADD PRIMARY KEY (`id_user`);
 
 -- DIRECCION
 
 ALTER TABLE `direccion`
     ADD PRIMARY KEY (`id_dir`);
-
--- FAVORITOS
-
-ALTER TABLE `favoritos`
-    ADD PRIMARY KEY (`id_fav`);
-
--- COMPRAS
-
-ALTER TABLE `compras`
-    ADD PRIMARY KEY (`id_compra`);
-
--- CARRITO
-
-ALTER TABLE `carrito`
-    ADD PRIMARY KEY (`id_carrito`);
 
 -- PRODUCTOS
 
@@ -451,20 +374,10 @@ ALTER TABLE `marcas`
 ALTER TABLE `devoluciones`
     ADD PRIMARY KEY (`id_dev`);
 
--- PAGOS
-
-ALTER TABLE `pagos`
-    ADD PRIMARY KEY (`id_pago`);
-
 -- MOTIVOS
 
 ALTER TABLE `motivos`
     ADD PRIMARY KEY (`id_motivo`);
-
--- CHATS
-
-ALTER TABLE `chats`
-    ADD PRIMARY KEY (`id_chat`);
 
 -- SUBASTAS
 
@@ -490,28 +403,10 @@ ALTER TABLE `players`
 -- ID's AUTOINCREMENTALES
 --
 
-ALTER TABLE `users`
-    MODIFY `id_user` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
-
 -- DIRECCION
 
 ALTER TABLE `direccion`
     MODIFY `id_dir` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=200;
-
--- FAVORITOS
-
-ALTER TABLE `favoritos`
-    MODIFY `id_fav` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=300;
-
--- COMPRAS
-
-ALTER TABLE `compras`
-    MODIFY `id_compra` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=400;
-
--- CARRITO
-
-ALTER TABLE `carrito`
-    MODIFY `id_carrito` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=500;
 
 -- PRODUCTOS
 
@@ -553,20 +448,10 @@ ALTER TABLE `marcas`
 ALTER TABLE `devoluciones`
     MODIFY `id_dev` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1300;
 
--- PAGOS
-
-ALTER TABLE `pagos`
-    MODIFY `id_pago` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1400;
-
 -- MOTIVOS
 
 ALTER TABLE `motivos`
     MODIFY `id_motivo` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1500;
-
--- CHATS
-
-ALTER TABLE `chats`
-    MODIFY `id_chat` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1600;
 
 -- SUBASTAS
 
@@ -592,28 +477,10 @@ ALTER TABLE `players`
 -- CLAVES AJENAS
 --
 
--- FAVORITOS
-
-ALTER TABLE `favoritos`
-    ADD CONSTRAINT `FK_user_fav` FOREIGN KEY (`user_fav`) REFERENCES `users` (`id_user`),
-    ADD CONSTRAINT `FK_producto_fav` FOREIGN KEY (`producto_fav`) REFERENCES `productos` (`id_producto`);
-
--- COMPRAS
-
-ALTER TABLE `compras`
-    ADD CONSTRAINT `FK_user_compra` FOREIGN KEY (`user_compra`) REFERENCES `users` (`id_user`),
-    ADD CONSTRAINT `FK_producto_compra` FOREIGN KEY (`producto_compra`) REFERENCES `productos` (`id_producto`);
-
--- CARRITO
-
-ALTER TABLE `carrito`
-    ADD CONSTRAINT `FK_user_carrito` FOREIGN KEY (`user_carrito`) REFERENCES `users` (`id_user`),
-    ADD CONSTRAINT `FK_producto_carrito` FOREIGN KEY (`producto_carrito`) REFERENCES `productos` (`id_producto`);
-
 -- PRODUCTOS
 
 ALTER TABLE `productos`
-    ADD CONSTRAINT `FK_id_vendedor` FOREIGN KEY (`id_vendedor`) REFERENCES `users` (`id_user`),
+    -- ADD CONSTRAINT `FK_id_vendedor` FOREIGN KEY (`id_vendedor`) REFERENCES `users` (`id_user`),
     ADD CONSTRAINT `FK_marca` FOREIGN KEY (`marca`) REFERENCES `marcas` (`id_marca`),
     ADD CONSTRAINT `FK_categoria` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`id_categoria`),
     ADD CONSTRAINT `FK_tipo` FOREIGN KEY (`tipo`) REFERENCES `tipo` (`id_tipo`);
@@ -631,32 +498,18 @@ ALTER TABLE `images`
 -- RESEÑAS
 
 ALTER TABLE `resenas`
-    ADD CONSTRAINT `FK_user_resena` FOREIGN KEY (`user_resena`) REFERENCES `users` (`id_user`),
+    -- ADD CONSTRAINT `FK_user_resena` FOREIGN KEY (`user_resena`) REFERENCES `users` (`id_user`),
     ADD CONSTRAINT `FK_producto_resena` FOREIGN KEY (`producto_resena`) REFERENCES `productos` (`id_producto`);
 
 -- DEVOLUCIONES
 
 ALTER TABLE `devoluciones`
-    ADD CONSTRAINT `FK_user_dev` FOREIGN KEY (`user_dev`) REFERENCES `users` (`id_user`),
+    -- ADD CONSTRAINT `FK_user_dev` FOREIGN KEY (`user_dev`) REFERENCES `users` (`id_user`),
     ADD CONSTRAINT `FK_producto_dev` FOREIGN KEY (`producto_dev`) REFERENCES `productos` (`id_producto`),
     ADD CONSTRAINT `FK_motivo` FOREIGN KEY (`motivo`) REFERENCES `motivos` (`id_motivo`);
-
--- PAGOS
-
-ALTER TABLE `pagos`
-    ADD CONSTRAINT `FK_metodo_pago` FOREIGN KEY (`metodo_pago`) REFERENCES `metodos_pago` (`id_metodo`),
-    ADD CONSTRAINT `FK_producto_pago` FOREIGN KEY (`producto_pago`) REFERENCES `productos` (`id_producto`),
-    ADD CONSTRAINT `FK_user_pago` FOREIGN KEY (`user_pago`) REFERENCES `users` (`id_user`);
-
--- CHATS
-
-ALTER TABLE `chats`
-    ADD CONSTRAINT `FK_cliente_chat` FOREIGN KEY (`cliente_chat`) REFERENCES `users` (`id_user`),
-    ADD CONSTRAINT `FK_vendedor_chat` FOREIGN KEY (`vendedor_chat`) REFERENCES `users` (`id_user`),
-    ADD CONSTRAINT `FK_producto_chat` FOREIGN KEY (`producto_chat`) REFERENCES `productos` (`id_producto`);
 
 -- SUBASTAS
 
 ALTER TABLE `subastas`
-    ADD CONSTRAINT `FK_user_sub` FOREIGN KEY (`user_sub`) REFERENCES `users` (`id_user`),
+    -- ADD CONSTRAINT `FK_user_sub` FOREIGN KEY (`user_sub`) REFERENCES `users` (`id_user`),
     ADD CONSTRAINT `FK_producto_sub` FOREIGN KEY (`producto_sub`) REFERENCES `productos` (`id_producto`);
