08ae1b3fba59887a5560e0908cd5f34d17433616
ImDenisc <danisatorrecucart@gmail.com>
2025-02-19 20:12:22 +0100

    CONT details inf

--- module/shop/model/ctrl_shop.js
+++ module/shop/model/ctrl_shop.js
@@ -30,7 +30,7 @@ function loadProductos(){
 function loadProductoDetails(id_producto){
     console.log("hola loadProductoDetails");
     // return false;
-    $('.container-productos').empty();
+    $('.container-productos').empty(); // vaciar todos los productos para dejar la web vacia y pintar el details
     // Limpiar el contenedor de detalles
     $('.container-details').empty();
     ajaxPromise('module/shop/ctrl/ctrl_shop.php?op=details&id_producto=' + id_producto, 'GET', 'JSON')
@@ -39,11 +39,14 @@ function loadProductoDetails(id_producto){
         // return false;
             $('<div></div>').attr('class', "details").attr({'id': data.id_producto}).appendTo('.container-details')
                 .html(
-                    "<img src = " + data.img_producto + " alt='foto' </img> " +
-                    "<div class='inf-producto'>" +
+                    "<div class='img-details'>"+
+                    "<img class='img-producto-border' src = " + data.img_producto + " alt='foto' </img> " +
+                    "</div>" + // end .img-details
+                    "<div class='inf-details'>" +
                     "<h3>" + data.nom_prod + "</h5>" +
-                    "<p class='precio'>" + data.precio + "€</p>" +
-                    "</div>"
+                    "<p class='precio-details'>" + data.precio + "€</p>" +
+                    "<p class='desc-details'>" + data.descripcion + "</p>" +
+                    "</div>" // end .inf-producto
                 ) // end .html
     }).catch(function(){
         window.location.href = "module/exceptions/ctrl/ctrl_exceptions.php?&op=503";
